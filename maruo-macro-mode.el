6286a416e46ef54443c84c2b5da1e4e6493855ae
USAMI Kenta <tadsan@zonu.me>
2016-05-06 07:17:47 +0900

    Update

--- maruo-macro-mode.el
+++ maruo-macro-mode.el
@@ -28,6 +28,8 @@
 
 ;;; Code:
 (require 'imenu)
+(eval-when-compile
+  (require 'rx))
 
 (defgroup maruo-macro nil
   "Hidemaru/Maruo editor macro script."
@@ -39,14 +41,76 @@
 (defcustom maruo-macro-mode-offset 4
   "Indentation of Maruo macro statement.")
 
+(defconst maruo-macro--re-syntax-keywords
+  (eval-when-compile
+    (rx (or line-start space ";" "}")
+        (group
+         (or "if" "else" "call" "return" "while" "break" "continue"
+             "goto" "endmacro" "endmacroall" "quit" "quitall"
+             "exit" "exitall" "saveexit" "saveexitall"))
+        (or space punctuation "(" "{")))
+  "Rexexp matching Maruo macro language keyword.")
+
 (defconst maruo-macro--re-keywords
   (eval-when-compile
-    (regexp-opt
-     '("if" "else" "call" "return" "while" "break" "continue"
-       "goto" "endmacro" "endmacroall" "quit" "quitall"
-       "exit" "exitall" "saveexit" "saveexitall"
-       "result" "yes" "no" "true" "false" "eof" "version" "platform")))
-  "Rexexp matching Maruo macro language keyword.")
+    (rx (not alphanumeric)
+        (group
+         (or "x" "y" "column" "lineno" "code" "unicode" "colorcode" "marked" "lineupdated"
+             "xpixel" "ypixel" "linecount" "linecount2" "linelen" "linelen2" "selecting"
+             "rectselecting" "lineselecting" "selectionlock" "mouseselecting"
+             "multiselecting" "inselecting" "seltopx" "seltopy" "selendx" "selendy"
+             "seltopcolumn" "seltoplineno" "selendcolumn" "selendlineno" "selopenx"
+             "selopeny" "windowwidth" "windowheight" "windowcx" "windowcy" "windowposx"
+             "windowposy" "splitstate" "splitmode" "windowstate" "windowstate2"
+             "cxscreen" "cyscreen" "xworkarea" "yworkarea" "cxworkarea" "cyworkarea"
+             "monitor" "monitorcount" "tabmode" "tabgroup" "tabgrouporder" "taborder"
+             "tabtotal" "tabgrouptotal" "screentopy" "compfilehandle" "scrolllinkhandle"
+             "filename" "basename" "directory" "filetype" "currentmacrofilename"
+             "currentmacrobasename" "currentmacrodirectory" "hidemarudir" "macrodir"
+             "settingdir" "backupdir" "windir" "winsysdir" "filename2" "basename2"
+             "directory2" "filename3" "basename3" "directory3" "overwrite" "updated"
+             "anyclipboard" "imestate" "browsemode" "keypressed" "replay" "hidemarucount"
+             "searchmode" "searchbuffer" "searchoption" "searchoption2" "replacebuffer"
+             "grepfilebuffer" "foundtopx" "foundtopy" "foundendx" "foundendy"
+             "foundhilighting" "foundbuffer" "foundoption" "readonly" "encode" "charset"
+             "bom" "loaddllfile" "getfocus" "event" "autocompstate" "argcount"
+             "fontname" "fontcharset" "fontsize" "fontmode" "showtab" "boldstate"
+             "showruler" "tabruler" "rulercolor" "rulerbackcolor" "width" "tabcount"
+             "linespace" "freecursor" "indentstate" "kinsokustate" "linenostate"
+             "pagestate" "correctlineno" "tcolor" "bcolor" "lcolor" "ccolor" "rcolor"
+             "hilightstate" "hilighttitle" "savewitheof" "ignoreeof" "backup"
+             "currentconfigset" "configstate" "formwidth" "formline" "getclipboard"
+             "date" "time" "tickcount" "year" "month" "day" "hour" "minute" "second"
+             "dayofweek" "dayofweeknum" "foldable" "folded" "rangeedittop" "rangeeditend"
+             "rangeeditmode" "outlinehandle"))
+        (not alphanumeric))))
+
+(defconst maruo-macro--re-important-symbols
+  (eval-when-compile
+    (rx (not alphanumeric)
+        (group
+         (or "result" "yes" "no" "true" "false" "eof" "version" "platform"))
+        (or space punctuation))))
+
+(defconst maruo-macro--re-functions
+  (eval-when-compile
+    (rx (or line-start space "=")
+        (group
+         (or "leftstr" "rightstr" "midstr" "strlen" "gettext" "gettext2" "char" "ascii"
+             "unichar" "unicode" "str" "val" "hex" "strstr" "strrstr" "getinistr"
+             "getininum" "input" "inputchar" "iskeydown" "getenv" "findhidemaru"
+             "hidemaruhandle" "hidemaruorder" "dderequest" "findwindow" "findwindowclass"
+             "sendmessage" "existfile" "getregstr" "getregnum" "getregbinary" "getconfig"
+             "getconfigcolor" "getfilehist" "getpathhist" "getsearchhist" "getreplacehist"
+             "gettagsfile" "columntox" "linenotoy" "xtocolumn" "ytolineno" "gettabhandle"
+             "getcurrenttab" "tolower" "toupper" "filter" "geteventparam" "getcolormarker"
+             "enumcolormarkerlayer" "gettitle" "getarg" "getstaticvariable" "seterrormode"
+             "getresultex" "getclipboardinfo" "charcount" "wcsleftstr" "wcsrightstr"
+             "wcsmidstr" "wcslen" "wcsstrstr" "wcsstrrstr" "byteindex_to_charindex"
+             "charindex_to_byteindex" "wideindex_to_charindex" "charindex_to_wideindex"
+             "getautocompitem" "geteventnotify"))
+        (zero-or-more space)
+        "(")))
 
 (defconst maruo-macro--re-variable-name
   "\\(\\(?:$$?\\|##?\\)[_A-Za-z][_A-Za-z0-1]*\\)"
@@ -65,7 +129,10 @@
    (cons maruo-macro--re-variable-name font-lock-variable-name-face)
    (cons maruo-macro--re-argument-variable font-lock-variable-name-face)
    (cons maruo-macro--re-subroutine-name font-lock-function-name-face)
-   (cons maruo-macro--re-keywords font-lock-keyword-face))
+   (cons maruo-macro--re-syntax-keywords (list 1 font-lock-keyword-face))
+   (cons maruo-macro--re-important-symbols (list 1 font-lock-type-face))
+   (cons maruo-macro--re-keywords (list 1 font-lock-builtin-face))
+   (cons maruo-macro--re-functions (list 1 font-lock-constant-face)))
   "Font lock keywords for Maruo macro.")
 
 (defconst maruo-macro-mode-syntax-table
@@ -122,11 +189,8 @@
 
   (setq-local indent-line-function 'indent-to-left-margin))
 
-;;;###autoload
-(defalias 'hidemaru-macro-mode 'maruo-macro-mode)
-
-;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.mac\\'" . maruo-macro-mode))
+;;;###autoload (defalias 'hidemaru-macro-mode 'maruo-macro-mode)
+;;;###autoload (add-to-list 'auto-mode-alist '("\\.mac\\'" . maruo-macro-mode))
 
 (provide 'maruo-macro-mode)
 ;;; maruo-macro-mode.el ends here
